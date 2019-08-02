4500ceb2e0471da52583b42e8e8c615f0be0af65
Timothy <hiitu7even@gmail.com>
2019-08-02 06:40:32 +0300

    Lab3

---
+++ W2Lab3/js/inheritance.js
@@ -0,0 +1,52 @@
+/*jshint esversion: 6 */
+
+(function () {
+    "use strict";
+
+    window.onload = function () {
+
+        String.prototype.filter = function(str){
+            let strArr = this.trim().split(" ");
+            let strip = strArr.filter(function (elem) {
+
+                    return elem !== str;
+            });
+
+            return strip.join(' ');
+
+        };
+
+        console.log("This house is not nice!".filter('not'));
+
+        Array.prototype.bubbleSort = function() {
+            const arr = this.slice(0);
+            if(this.length<2){
+                return this;
+            }
+
+            let swap = function(b){
+                [arr[b-1],arr[b]]=[arr[b],arr[b-1]];
+            };
+
+            for(let i =0; i<this.length;i++){
+                for(let j =0; j<arr.length-i;j++) {
+
+                    if (arr[j] < arr[j-1]) {
+                        console.log(arr[j]);
+                        swap(j);
+                    }
+                }
+
+            }
+
+            return arr;
+
+
+        };
+
+        console.log([6,4,0, 3,-2,1].bubbleSort());
+
+    };
+
+
+})();
